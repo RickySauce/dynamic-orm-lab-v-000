081510ba3888f3597ac79e0277ea8a675d031454
Eric DeSimone <eric.desimone@cortland.edu>
2018-06-26 12:23:51 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -16,7 +16,6 @@ class InteractiveRecord
     table_info = DB[:conn].execute(sql)
     column_names = []
     table_info.each do |row|
-      binding.pry
       column_names << row["name"]
     end
     column_names.compact
